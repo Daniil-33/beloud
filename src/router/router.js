96703c914fae34e37fa7b3cf7fbbe5cd225d0643
Daniil33 <daniilkostyuk33@gmail.com>
2023-04-02 13:21:10 +0300

    navigation auth guard

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Auth from '@/views/Auth.vue'
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [],
+	routes: [
+		{
+			name: 'Auth',
+			path: '/auth',
+			component: Auth,
+		}
+	],
 })
+
+import { useAuthModule } from '@/modules/Auth/'
+
+router.beforeEach(async (to, from) => {
+	const { isAuth } = useAuthModule()
+
+	debugger
+	if (
+	  // make sure the user is authenticated
+	  !isAuth.value &&
+	  // ❗️ Avoid an infinite redirect
+	  to.name !== 'Auth'
+	) {
+	  // redirect the user to the login page
+	  return { name: 'Auth' }
+	}
+  })
 
 export default router
