2edcdf592474f83a118f5e47d1a737f323044d7c
Daniil33 <daniilkostyuk33@gmail.com>
2023-04-03 06:40:47 +0300

    Songs module

---
+++ src/modules/Songs/components/PopularNowSongs.vue
@@ -0,0 +1,79 @@
+<template>
+	<CardTileList :items="songs" >
+		<template #default="{ item, index }">
+			<SongCard :cardData="item"/>
+		</template>
+	</CardTileList>
+</template>
+<script>
+import CardTileList from './UI/CardTileList.vue'
+import SongCard from './UI/SongCard.vue'
+
+import { ref } from 'vue'
+
+export default {
+	name: 'PopularNowSongs',
+	components: {
+		CardTileList,
+		SongCard
+	},
+	setup() {
+		const songs = ref([
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit',
+				groupName: 'Nirvana 2',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 3',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit',
+				groupName: 'Nirvana 5',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 6',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 7',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 8',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 9',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 4',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 4',
+				image: '/images/song-image.jpg'
+			},
+		])
+
+		return {
+			songs
+		};
+	}
+}
+</script>
---
+++ src/modules/Songs/components/RecentlyListenedSongs.vue
@@ -0,0 +1,88 @@
+<template>
+	<CardSwiper
+		:items="songs"
+	>
+		<template #default="{ item }">
+			<SongCard :cardData="item"></SongCard>
+		</template>
+	</CardSwiper>
+</template>
+<script>
+import SongCard from './UI/SongCard.vue'
+import CardSwiper from './UI/CardSwiper.vue'
+
+import { ref } from 'vue'
+
+export default {
+	name: 'RecentlyListenedSongs',
+	props: {
+		songs: {
+			type: Array,
+			required: true,
+			default: () => []
+		}
+	},
+	components: {
+		SongCard,
+		CardSwiper
+	},
+	setup() {
+		const songs = ref([
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit',
+				groupName: 'Nirvana 2',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 3',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit',
+				groupName: 'Nirvana 5',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 6',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 7',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 8',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 9',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 4',
+				image: '/images/song-image.jpg'
+			},
+			{
+				title: 'Smells Like Teen Spirit ',
+				groupName: 'Nirvana 4',
+				image: '/images/song-image.jpg'
+			},
+		])
+
+		return {
+			songs
+		};
+	}
+}
+</script>
