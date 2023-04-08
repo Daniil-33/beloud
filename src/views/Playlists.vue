464e8a32fa92eaa74ceaa7c78e7464f609cbb67e
Oleksandr <sasha15200201@gmail.com>
2023-04-08 17:18:43 +0300

    router playlists

---
+++ src/views/Playlists.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="playlists_wrapper">
+        <div class="playlists_playlist">
+
+        </div>
+        <div class="playlist__list-wrapper">
+            .playlist__list-list
+        </div>
+    </div>
+</template>
